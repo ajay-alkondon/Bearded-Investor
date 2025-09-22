2a95174512d63c93a776293418275a84897a9098
Ajay Alkondon <coderarityco@gmail.com>
2025-09-21 19:40:10 -0500

    dsf

--- wp-content/plugins/journey-to-wealth/public/assets/js/public-scripts.js
+++ wp-content/plugins/journey-to-wealth/public/assets/js/public-scripts.js
@@ -515,7 +515,7 @@ function initializeRevenueChart() {
                                 content: 'Past',
                                 color: '#aaa',
                                 font: { size: 12 },
-                                xAdjust: 0,
+                                xAdjust: -100,
                                 yAdjust: 0,
                                 textAlign: 'right',
                             },
@@ -527,7 +527,7 @@ function initializeRevenueChart() {
                                 content: 'Analysts Forecasts',
                                 color: '#aaa',
                                 font: { size: 12 },
-                                xAdjust: 0,
+                                xAdjust: 100,
                                 yAdjust: 0,
                                 textAlign: 'left',
                             }
